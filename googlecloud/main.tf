049df08200cfcd7f66bfe3a7a489d2855e90aa23
sumanshnkr <sumanshankr@gmail.com>
2025-02-14 18:02:45 +0530

    edited bucket name

--- googlecloud/main.tf
+++ googlecloud/main.tf
@@ -1,4 +1,4 @@
-resource "google_storage_bucket" "bucket" {
+resource "google_storage_bucket" "bucket2" {
   name          = var.bucket_name
   location      = var.region
   force_destroy = true # Optional: Set to false for production
